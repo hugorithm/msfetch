6e3a48903a2afd788294b0789371a6f825190639
Hugo Silva <hugofsilv@gmail.com>
2024-01-10 15:26:11 +0000

    Add help handler

--- main.go
+++ main.go
@@ -11,30 +11,36 @@ func main() {
 	query := searchCmd.String("q", "", "Search query")
 
 	if len(os.Args) < 2 {
-		fmt.Println("expected 'search' command")
+		fmt.Println("expected 'search' or '--help' command")
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
 	case "search":
 		HandleSearch(searchCmd, query)
+	case "--help":
+		HandleHelp()
 	default:
-		fmt.Println("unexpected command, type --help for commands")
+		fmt.Println("unexpected command, type '--help' for commands")
 		os.Exit(1)
 	}
-
 }
 
 func HandleSearch(searchCmd *flag.FlagSet, query *string) {
 	searchCmd.Parse(os.Args[2:])
 
 	if *query == "" {
-		fmt.Print("Search query is required")
+		fmt.Print("Search query is required\n\n")
 		searchCmd.PrintDefaults()
 		os.Exit(1)
 	}
 
 	search := *query
-
 	fmt.Print(search)
+}
+
+func HandleHelp() {
+	fmt.Println("Usage:")
+	fmt.Println("  search -q <query>")
+	os.Exit(0)
 }
